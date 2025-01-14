23730cde8dacf708c9cd2415774073c97e39af5b
Th3F4nd1t <indephisive@gmail.com>
2025-01-14 12:49:06 -0500

    Add CSP EXAM comments

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from parts import *
 CYAN_VERSION = 1
 CONFIG_PATH = ".\configGroup\config.json"
 
+# Get the config from the json file
 config = getConfig(CONFIG_PATH)
 
+# Get the processor object
 proc = Processor(config)
 
+# Load the program to be ran
 proc.loadProgram("./program.txt")
 
+# Run the program
 proc.run()
