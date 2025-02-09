772bca9a675c056d832ff5f9c398b74c81fac46e
Th3F4nd1t <indephisive@gmail.com>
2025-02-09 17:16:08 -0500

    put scripting on hold, redoing engine next

--- configCreator/scripting.py
+++ configCreator/scripting.py
@@ -14,32 +14,32 @@ def convertToPythonInstruction(input: str) -> str:
 
     """
 
-#### Memory
-- `setReg(address: int, data: int, bool setFlags) -> none`
-- `setRAM(address: int, data: int, bool setFlags) -> none`
-- `setCustomReg(name: str, data: int, bool setFlags) -> none`
-- `getReg(address: int) -> int`
-- `getRAM(address: int) -> int`
-- `getProm(address: int) -> int`
-- `getCustomReg(address: int) -> int`
+# #### Memory
+# - `setReg(address: int, data: int, bool setFlags) -> none`
+# - `setRAM(address: int, data: int, bool setFlags) -> none`
+# - `setCustomReg(name: str, data: int, bool setFlags) -> none`
+# - `getReg(address: int) -> int`
+# - `getRAM(address: int) -> int`
+# - `getProm(address: int) -> int`
+# - `getCustomReg(address: int) -> int`
 
-### Flags
-- `getFlag(name: str) -> bool`
+# ### Flags
+# - `getFlag(name: str) -> bool`
 
-#### Input/Output
-- `setIO(address: int, data: int) -> none`
-- `getIO(address: int) -> int`
-- `setIOLock(address: int, lockState: bool) -> none`
+# #### Input/Output
+# - `setIO(address: int, data: int) -> none`
+# - `getIO(address: int) -> int`
+# - `setIOLock(address: int, lockState: bool) -> none`
 
-#### Program Counter
-- `getPC() -> int`
-- `setPC(address: int) -> none`
-- `offsetPC(offset: int) -> none`
-- `incrementPC() -> none`
+# #### Program Counter
+# - `getPC() -> int`
+# - `setPC(address: int) -> none`
+# - `offsetPC(offset: int) -> none`
+# - `incrementPC() -> none`
 
-#### Internal Methods (DO NOT USE)
-- `initState() -> none`
-- `initFlags() -> none`
-- `updateFlags() -> none`
-- `executeLine() -> none`
-- `execute() -> bool`
+# #### Internal Methods (DO NOT USE)
+# - `initState() -> none`
+# - `initFlags() -> none`
+# - `updateFlags() -> none`
+# - `executeLine() -> none`
+# - `execute() -> bool`
