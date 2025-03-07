0e2f5c559a999bee6209166fb295209336b1ae6a
Alexander Edwards <bdaace09@gmail.com>
2025-03-06 20:27:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ proc = Processor(config)
 
 proc.loadProgram("./program.txt")
 
-proc.runSteps() # Use proc.runSteps() to step through the program
+proc.run() # Use proc.runSteps() to step through the program
